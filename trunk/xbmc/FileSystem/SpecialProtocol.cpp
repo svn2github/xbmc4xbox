svn2github/xbmc4xbox

commit 38bb44d4e75e0d0e97f6042b88f4ac7bb73c4b71

merged: xbmc mainline/trunk revisions 31455,33380,33381,33384,33393,33399,33425,33426,33448,33449,33450,33451,33452 33455,33458,33498,33501,33502,33505,33520-33525,33529-33531,33533,33535,33537 33542,33553,33560,33561,33562,33563,33569,33579
git-svn-id: https://svn.exotica.org.uk:8443/xbmc4xbox@30607 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

trunk/xbmc/FileSystem/SpecialProtocol.cpp
@@ -78,7 +78,11 @@ bool CSpecialProtocol::ComparePath(const CStdString &path1, const CStdString &pa
 CStdString CSpecialProtocol::TranslatePath(const CStdString &path)
 {
   CURL url(path);
+  return TranslatePath(url);
+}
 
+CStdString CSpecialProtocol::TranslatePath(const CURL &url)
+{
   // check for special-protocol, if not, return
   if (!url.GetProtocol().Equals("special"))
   {
@@ -90,7 +94,7 @@ CStdString CSpecialProtocol::TranslatePath(const CStdString &path)
     }
 #endif
 
-    return path;
+    return url.Get();
   }
 
   CStdString FullFileName = url.GetFileName();

trunk/xbmc/Util.cpp
@@ -633,7 +633,7 @@ void CUtil::RemoveExtension(CStdString& strFileName)
   }
 }
 
-void CUtil::CleanString(CStdString& strFileName, CStdString& strTitle, CStdString& strTitleAndYear, CStdString& strYear, bool bRemoveExtension /* = false */)
+void CUtil::CleanString(const CStdString& strFileName, CStdString& strTitle, CStdString& strTitleAndYear, CStdString& strYear, bool bRemoveExtension /* = false */, bool bCleanChars /* = true */)
 {
   strTitleAndYear = strFileName;
 
@@ -642,7 +642,8 @@ void CUtil::CleanString(CStdString& strFileName, CStdString& strTitle, CStdStrin
 
   const CStdStringArray &regexps = g_advancedSettings.m_videoCleanStringRegExps;
 
-  CRegExp reTags, reYear;
+  CRegExp reTags(true);
+  CRegExp reYear;
   CStdString strExtension;
   GetExtension(strFileName, strExtension);
 
@@ -669,7 +670,7 @@ void CUtil::CleanString(CStdString& strFileName, CStdString& strTitle, CStdStrin
       continue;
     }
     int j=0;
-    if ((j=reTags.RegFind(strFileName.ToLower().c_str())) > 0)
+    if ((j=reTags.RegFind(strFileName.c_str())) > 0)
       strTitleAndYear = strTitleAndYear.Mid(0, j);
   }
 
@@ -678,6 +679,7 @@ void CUtil::CleanString(CStdString& strFileName, CStdString& strTitle, CStdStrin
   // if the file contains no spaces, all '.' tokens should be replaced by
   // spaces - one possibility of a mistake here could be something like:
   // "Dr..StrangeLove" - hopefully no one would have anything like this.
+  if (bCleanChars)
   {
     bool initialDots = true;
     bool alreadyContainsSpace = (strTitleAndYear.Find(' ') >= 0);
@@ -1505,6 +1507,9 @@ bool CUtil::IsOnLAN(const CStdString& strPath)
   if(!IsRemote(strPath))
     return false;
 
+  if(IsPlugin(strPath))
+    return false;
+
   CStdString host = url.GetHostName();
   if(host.length() == 0)
     return false;
@@ -1695,9 +1700,9 @@ bool CUtil::IsFTP(const CStdString& strFile)
          url.GetTranslatedProtocol() == "ftps";
 }
 
-bool CUtil::IsInternetStream(const CStdString& strFile, bool bStrictCheck /* = false */)
+bool CUtil::IsInternetStream(const CURL& url, bool bStrictCheck /* = false */)
 {
-  CURL url(strFile);
+  
   CStdString strProtocol = url.GetProtocol();
   
   if (strProtocol.IsEmpty())
@@ -1705,10 +1710,7 @@ bool CUtil::IsInternetStream(const CStdString& strFile, bool bStrictCheck /* = f
 
   // there's nothing to stop internet streams from being stacked
   if (strProtocol == "stack")
-  {
-    CStdString strFile2 = CStackDirectory::GetFirstStackedFile(strFile);
-    return IsInternetStream(strFile2);
-  }
+    return IsInternetStream(CStackDirectory::GetFirstStackedFile(url.Get()));
 
   CStdString strProtocol2 = url.GetTranslatedProtocol();
 

trunk/guilib/GUIScrollBarControl.cpp
@@ -195,7 +195,7 @@ void CGUIScrollBar::UpdateBarSize()
   if (m_orientation == VERTICAL)
   {
     // calculate the height to display the nib at
-    float percent = (float)m_pageSize / m_numItems;
+    float percent = (m_numItems == 0) ? 0 : (float)m_pageSize / m_numItems;
     float nibSize = GetHeight() * percent;
     if (nibSize < m_guiNibFocus.GetTextureHeight() + 2 * MIN_NIB_SIZE) nibSize = m_guiNibFocus.GetTextureHeight() + 2 * MIN_NIB_SIZE;
     if (nibSize > GetHeight()) nibSize = GetHeight();
@@ -220,7 +220,7 @@ void CGUIScrollBar::UpdateBarSize()
   else
   {
     // calculate the height to display the nib at
-    float percent = (float)m_pageSize / m_numItems;
+    float percent = (m_numItems == 0) ? 0 : (float)m_pageSize / m_numItems;
     float nibSize = GetWidth() * percent + 0.5f;
     if (nibSize < m_guiNibFocus.GetTextureWidth() + 2 * MIN_NIB_SIZE) nibSize = m_guiNibFocus.GetTextureWidth() + 2 * MIN_NIB_SIZE;
     if (nibSize > GetWidth()) nibSize = GetWidth();
@@ -231,7 +231,7 @@ void CGUIScrollBar::UpdateBarSize()
     m_guiNibFocus.SetWidth(nibSize);
 
     // and the position
-    percent = (float)m_offset / (m_numItems - m_pageSize);
+    percent = (m_numItems == m_pageSize) ? 0 : (float)m_offset / (m_numItems - m_pageSize);
     float nibPos = (GetWidth() - nibSize) * percent;
     if (nibPos < 0) nibPos = 0;
     if (nibPos > GetWidth() - nibSize) nibPos = GetWidth() - nibSize;

trunk/guilib/SkinInfo.h
@@ -81,6 +81,7 @@ public:
 protected:
   void LoadIncludes();
   bool LoadStartupWindows(const TiXmlElement *startup);
+  bool IsWide(RESOLUTION res) const;
 
   wchar_t credits[6][CREDIT_LINE_LENGTH];  // credits info
   int m_iNumCreditLines;  // number of credit lines

trunk/xbmc/AdvancedSettings.cpp
@@ -119,8 +119,8 @@ CAdvancedSettings::CAdvancedSettings()
   // in a flat dir structure, but is perfectly safe in a dir-per-vid one.
   //m_videoStackRegExps.push_back("(.*?)([ ._-]*[0-9])(.*?)(\\.[^.]+)$");
 
-  // foo.s01.e01, foo.s01_e01, S01E02 foo
-  m_tvshowStackRegExps.push_back(TVShowRegexp(false,"[Ss]([0-9]+)[][._-]*[Ee]([0-9]+)([^\\\\/]*)$"));
+  // foo.s01.e01, foo.s01_e01, S01E02 foo, S01 - E02
+  m_tvshowStackRegExps.push_back(TVShowRegexp(false,"[Ss]([0-9]+)[][._- ]*[Ee]([0-9]+)([^\\\\/]*)$"));
   // foo.ep01, foo.EP_01
   m_tvshowStackRegExps.push_back(TVShowRegexp(false,"[\\._ -]()[Ee][Pp]_?([0-9]+)([^\\\\/]*)$"));
   // foo.yyyy.mm.dd.* (byDate=true)

trunk/xbmc/Application.cpp
@@ -3741,12 +3741,9 @@ bool CApplication::PlayFile(const CFileItem& item, bool bRestart)
 
   if (item.IsPlugin())
   { // we modify the item so that it becomes a real URL
-    CFileItem item_new;
+    CFileItem item_new(item);
     if (DIRECTORY::CPluginDirectory::GetPluginResult(item.m_strPath, item_new))
-    {
-      item_new.SetProperty("original_listitem_url", item.HasProperty("original_listitem_url") ? item.GetProperty("original_listitem_url") : item.m_strPath);
       return PlayFile(item_new, false);
-    }
     return false;
   }
 
@@ -4351,12 +4348,13 @@ void CApplication::StopPlaying()
       m_pCdgParser->Stop();
 #endif
 
-    // turn off visualisation window when stopping
-    if (iWin == WINDOW_VISUALISATION)
-      g_windowManager.PreviousWindow();
-
     if (m_pPlayer)
       m_pPlayer->CloseFile();
+
+    // turn off visualisation window when stopping
+    if (iWin == WINDOW_VISUALISATION
+    ||  iWin == WINDOW_FULLSCREEN_VIDEO)
+      g_windowManager.PreviousWindow();
 
     g_partyModeManager.Disable();
   }

trunk/xbmc/Favourites.cpp
@@ -148,24 +148,31 @@ bool CFavourites::IsFavourite(CFileItem *item, int contextWindow)
   return items.Contains(GetExecutePath(item, contextWindow));
 }
 
+static CStdString Paramify(const CStdString& param)
+{
+  CStdString result(param);
+  result.Replace("\"", "\\\"");
+  return "\"" + result + "\"";
+}
+
 CStdString CFavourites::GetExecutePath(const CFileItem *item, int contextWindow)
 {
   CStdString execute;
   if (item->m_bIsFolder && (g_advancedSettings.m_playlistAsFolders ||
                             !(item->IsSmartPlayList() || item->IsPlayList())))
-    execute.Format("ActivateWindow(%i,%s)", contextWindow, item->m_strPath);
+    execute.Format("ActivateWindow(%i,%s)", contextWindow, Paramify(item->m_strPath));
   else if (item->m_strPath.Left(9).Equals("plugin://"))
-    execute.Format("RunPlugin(%s)", item->m_strPath);
+    execute.Format("RunPlugin(%s)", Paramify(item->m_strPath));
   else if (contextWindow == WINDOW_SCRIPTS)
     execute.Format("RunScript(%s)", item->m_strPath);
   else if (contextWindow == WINDOW_PROGRAMS)
-    execute.Format("RunXBE(%s)", item->m_strPath);
+    execute.Format("RunXBE(%s)", Paramify(item->m_strPath));
   else  // assume a media file
   {
     if (item->IsVideoDb() && item->HasVideoInfoTag())
-      execute.Format("PlayMedia(%s)", item->GetVideoInfoTag()->m_strFileNameAndPath);
+      execute.Format("PlayMedia(%s)", Paramify(item->GetVideoInfoTag()->m_strFileNameAndPath));
     else
-      execute.Format("PlayMedia(%s)", item->m_strPath);
+      execute.Format("PlayMedia(%s)", Paramify(item->m_strPath));
   }
   return execute;
 }

trunk/xbmc/FileSystem/CacheMemBuffer.cpp
@@ -184,7 +184,7 @@ __int64 CacheMemBuffer::Seek(__int64 iFilePosition, int iWhence)
   }
 
   __int64 iHistoryStart = m_nStartPosition - m_HistoryBuffer.GetMaxReadSize();
-  if (iFilePosition < m_nStartPosition && iFilePosition > iHistoryStart)
+  if (iFilePosition < m_nStartPosition && iFilePosition >= iHistoryStart)
   {
     CRingBuffer saveHist, saveUnRead;
     __int64 nToSkip = iFilePosition - iHistoryStart;

commit 3060d6064a0953a1e572a92b6ba582b0d8f5f575

fixed: DVD Playback was broken by r30607
git-svn-id: https://svn.exotica.org.uk:8443/xbmc4xbox@30633 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

trunk/xbmc/FileSystem/SpecialProtocol.cpp
@@ -78,6 +78,11 @@ bool CSpecialProtocol::ComparePath(const CStdString &path1, const CStdString &pa
 CStdString CSpecialProtocol::TranslatePath(const CStdString &path)
 {
   CURL url(path);
+  if (!url.GetProtocol().Equals("special"))
+  {
+    return path;
+  }
+  
   return TranslatePath(url);
 }
 

trunk/xbmc/URL.cpp
@@ -79,9 +79,10 @@ void CURL::Parse(const CStdString& strURL1)
   if (!strURL.size()) return ;
   if (strURL.Equals("?", true)) return;
 
-  if (strURL.size() > 1 && strURL[1] == ':')
+  if (strURL.size() > 1 && strURL.Find(':') > 0 && !CUtil::IsURL(strURL))
   {
     // form is drive:directoryandfile
+    // Note that drive can be more than one character, like with cdrom: !
 
     // set filename and update extension
 

trunk/xbmc/Util.cpp
@@ -3357,6 +3357,10 @@ CStdString CUtil::ValidatePath(const CStdString &path, bool bFixDoubleSlashes /*
   if (CUtil::IsDOSPath(path))
   {
     result.Replace('/', '\\');
+    /* The double slash correction should only be used when *absolutely*
+       necessary! This applies to certain DLLs or use from Python DLLs/scripts
+       that incorrectly generate double (back) slashes.
+    */
     if (bFixDoubleSlashes)
     {
       // Fixup for double back slashes (but ignore the \\ of unc-paths)
@@ -3370,6 +3374,10 @@ CStdString CUtil::ValidatePath(const CStdString &path, bool bFixDoubleSlashes /*
   else if (path.Find("://") >= 0 || path.Find(":\\\\") >= 0)
   {
     result.Replace('\\', '/');
+    /* The double slash correction should only be used when *absolutely*
+       necessary! This applies to certain DLLs or use from Python DLLs/scripts
+       that incorrectly generate double (back) slashes.
+    */
     if (bFixDoubleSlashes)
     {
       // Fixup for double forward slashes(/) but don't touch the :// of URLs